69391e4e6e3a119daf7b1596f420151a2d1709bc
Julie Toke <41884050+JulieToke@users.noreply.github.com>
2022-03-25 22:37:50 +1100

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,21 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Rude Gnome",
+    desc : "Keep the vistors at bay with this rude little fellow!",
+    img : "gardengnome1.jpg",
+    price : 25
+  },
+  124: {
+    name : "Bluebird Gnome",
+    desc : "Welcome guests with this jolly animal loving chap!",
+    img : "gardengnome2.jpg",
+    price : 25
+  },
+  125: {
+    name : "Gangsta Gnome",
+    desc : "Discourage intruders with this tough guy!.",
+    img : "gardengnome3.jpg",
+    price : 25
+  },
+};
